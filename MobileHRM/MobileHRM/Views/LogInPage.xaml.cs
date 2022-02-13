7e05377d09e5772d8a53896b16d35c986be0167d
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-13 09:30:38 +0330

    Add Hash Password and Add Err Msg for noInput

--- MobileHRM/MobileHRM/Views/LogInPage.xaml.cs
+++ MobileHRM/MobileHRM/Views/LogInPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using System.Security.Cryptography;
+
 namespace MobileHRM.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -19,5 +21,34 @@ namespace MobileHRM.Views
             InitializeComponent();
         }
 
+        private void LogIn_Btn_Clk(object sender, EventArgs e)
+        {
+            if (pass.Text == "" || email.Text == "" || pass.Text == null || email.Text == null)
+                   new Popup.ShowMsgPopup("Please enter your username and password" , "Error" , 3);
+            else
+            {
+            string hashPass = ComputeSha256Hash(pass.Text);
+            
+
+            }
+        }
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
