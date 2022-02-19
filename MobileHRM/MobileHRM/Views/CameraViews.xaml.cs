1d76eb8b653a8f7697b8c75298c37cb24e9140e8
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-19 10:58:10 +0330

    Add Camera View for take img

---
+++ MobileHRM/MobileHRM/Views/CameraViews.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHRM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraViews : ContentPage
+    {
+        public CameraViews()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false); 
+            
+        }
+
+        private void CaptureImage(object sender, EventArgs e)
+        {
+            xctCameraView.Shutter();
+        }
+        private void RecordVideo(object sender, EventArgs e)
+        {
+            xctCameraView.Shutter();
+            btnrecordVideo.IsEnabled = false;
+            btnstopVideo.IsEnabled = true;
+        }
+        private void StopVideo(object sender, EventArgs e)
+        {
+            xctCameraView.Shutter();
+            btnrecordVideo.IsEnabled = true;
+            btnstopVideo.IsEnabled = false;
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (xctCameraView.CaptureMode == CameraCaptureMode.Photo)
+            {
+                xctCameraView.CaptureMode = CameraCaptureMode.Video;
+
+                captureBtn.IsEnabled = false;
+                btnrecordVideo.IsEnabled = true;
+                btnstopVideo.IsEnabled = false;
+            }
+            else
+            {
+                xctCameraView.CaptureMode = CameraCaptureMode.Photo;
+
+                captureBtn.IsEnabled = true;
+                btnrecordVideo.IsEnabled = false;
+                btnstopVideo.IsEnabled = false;
+            }
+        }
+
+        private async void MediaCaptured(object sender, MediaCapturedEventArgs e)
+        {
+
+            if (xctCameraView.CaptureMode == CameraCaptureMode.Photo)
+            {
+                ImageEdit imgEdit = new ImageEdit(e.Image);
+                Navigation.PushAsync(imgEdit);
+            }
+            //else
+            //{
+            //    imgView.Source = e.Video.File;
+            //    imgView.IsAnimationPlaying = true;
+            //    imgViewPanel.IsVisible = true;
+            //}
+
+            //string str;
+            //if (xctCameraView.CaptureMode == CameraCaptureMode.Photo)
+            //    str = e.Image.ToString();
+            //else
+            //    str = e.Video.File;
+
+        }
+
+        private void CloseImageView(object sender, EventArgs e)
+        {
+            //imgViewPanel.IsVisible = false;
+        }
+
+        private void switchCam_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (xctCameraView.CameraOptions == CameraOptions.Back)
+                xctCameraView.CameraOptions = CameraOptions.Front;
+            else
+                xctCameraView.CameraOptions = CameraOptions.Back;
+
+            zoomslider.Maximum = xctCameraView.MaxZoom;
+        }
+
+    }
+}
