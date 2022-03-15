bfe736b424203e3b36f85f15d80f18467316dc54
Moein13r <mrahmat1386@gmail.com>
2022-03-15 00:43:58 -0700

    Added New Refrence Popup And New Tag popup

---
+++ MobileHRM/MobileHRM/Views/Popup/NewReference.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHRM.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewReference : PopupPage
+    {
+        public NewReference()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Close_Imagebutton(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
---
+++ MobileHRM/MobileHRM/Views/Popup/NewTag.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHRM.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTag : PopupPage
+    {
+        public NewTag()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Close_Imagebutton(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
