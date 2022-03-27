8216271a130158837cb3e82b30a45866bb66c402
vahidzack <98702356+vahidzack@users.noreply.github.com>
2022-03-27 15:54:19 +0430

    Add new page chart and overview

---
+++ MobileHRM/MobileHRM/Views/Accounting_Report.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHRM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Accounting_Report : TabbedPage
+    {
+        public Accounting_Report()
+        {
+            InitializeComponent();
+            Children.Add(new Accounting_Report_Chart());
+            Children.Add(new Accounting_Report_OverView());
+        }
+    }
+}
---
+++ MobileHRM/MobileHRM/Views/Accounting_Report_Chart.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHRM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Accounting_Report_Chart : ContentPage
+    {
+        public Accounting_Report_Chart()
+        {
+            InitializeComponent();
+            List<ChartEntry> entries = new List<ChartEntry>()
+            {
+                new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                               new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                               new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                 new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                  new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                   new ChartEntry(212)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                    new ChartEntry(10)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                },
+                     new ChartEntry(2)
+                {
+                    Label = "UWP",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2c3e50")
+                   
+                    
+                }
+            };
+            charview.Chart = new BarChart { Entries = entries };
+            charview1.Chart = new PieChart { Entries = entries };
+        }
+    }
+}
