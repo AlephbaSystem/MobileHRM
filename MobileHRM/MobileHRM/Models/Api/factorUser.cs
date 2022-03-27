8b8171ace337c1edba2e42aaef805751d1d6d774
vahidzack <98702356+vahidzack@users.noreply.github.com>
2022-03-27 09:56:45 +0430

    creat model

---
+++ MobileHRM/MobileHRM/Models/Api/factorUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileHRM.Models.Api
+{
+    internal class factorUser
+    {
+        public int id { get; set; }    
+        public string name { get; set; }    
+        public string employeeId { get; set; }    
+        public int type { get; set; }    
+        public string address { get; set; }    
+        public string email { get; set; }    
+        public int phoneNumber { get; set; }    
+        public string city { get; set; }    
+    }
+}
---
+++ MobileHRM/MobileHRM/Models/Api/invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileHRM.Models.Api
+{
+    internal class invoice
+    {
+        public int id { get; set; } 
+        public int factor { get; set; } 
+        public int from { get; set; } 
+        public string about { get; set; } 
+        public string details { get; set; } 
+        public DateTime date { get; set; } 
+        public string reciver { get; set; } 
+        public decimal amount { get; set; } 
+    }
+}
