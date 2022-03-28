302d16cae1e6852ff1f6be511ca79f7a536b04b8
vahidzack <98702356+vahidzack@users.noreply.github.com>
2022-03-28 12:17:31 +0430

    finaly chart created

---
+++ MobileHRM/MobileHRM/Converter/CalanderConvert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileHRM.Converter
+    {
+        public class CalanderConvert : IValueConverter
+        {
+
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if (value == null)
+                    return string.Empty;
+
+                var datetime = (DateTime)value;
+                //put your custom formatting here
+                return datetime.ToLocalTime().ToString("M yyyy");
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
