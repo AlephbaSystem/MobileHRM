17652c9c46c0c2318ac38a3628b2aac22412eeb0
movahhedizadeh <mohammadmovahhedizadeh@gmail.com>
2022-04-04 14:54:40 +0430

    fix #9

--- MobileHRM/MobileHRM/Converter/DateConvert.cs
+++ MobileHRM/MobileHRM/Converter/DateConvert.cs
@@ -9,7 +9,7 @@ namespace MobileHRM.Converter
     public class DateConvert : IValueConverter
     {
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
                 return string.Empty;
@@ -25,7 +25,7 @@ namespace MobileHRM.Converter
             }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
