cc0c42143f20a804b2a916b1d72b8d290d179a0d
Moein13r <mrahmat1386@gmail.com>
2022-04-13 01:36:44 -0700

    Fixed #7

--- MobileHRM/MobileHRM/Models/Entities/Attachment.cs
+++ MobileHRM/MobileHRM/Models/Entities/Attachment.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace MobileHRM.Models.Entities
 {
     public class Attachment
-    {
+    {        
         public string mediaType { get; set; }
         public byte[] media { get; set; }        
     }
